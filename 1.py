1758577d077aa6bfa131c015095587cd49c19a5b
neil li <399109872@qq.com>
2018-01-16 21:14:09 +0800

    create 1.py

---
+++ 1.py
@@ -0,0 +1,88 @@
+"""
+Know more, visit my Python tutorial page: https://morvanzhou.github.io/tutorials/
+My Youtube Channel: https://www.youtube.com/user/MorvanZhou
+Dependencies:
+tensorflow: 1.1.0
+"""
+import tensorflow as tf
+
+var = tf.Variable(0)    # our first variable in the "global_variable" set
+
+add_operation = tf.add(var, 1)
+update_operation = tf.assign(var, add_operation)
+
+with tf.Session() as sess:
+    # once define variables, you have to initialize them by doing this
+    sess.run(tf.global_variables_initializer())
+    for _ in range(3):
+        sess.run(update_operation)
+        print(sess.run(var))
+
+		
+"""
+Know more, visit my Python tutorial page: https://morvanzhou.github.io/tutorials/
+My Youtube Channel: https://www.youtube.com/user/MorvanZhou
+Dependencies:
+tensorflow: 1.1.0
+"""
+import tensorflow as tf
+
+var = tf.Variable(0)    # our first variable in the "global_variable" set
+
+add_operation = tf.add(var, 1)
+update_operation = tf.assign(var, add_operation)
+
+with tf.Session() as sess:
+    # once define variables, you have to initialize them by doing this
+    sess.run(tf.global_variables_initializer())
+    for _ in range(3):
+        sess.run(update_operation)
+        print(sess.run(var))
+		
+"""
+Know more, visit my Python tutorial page: https://morvanzhou.github.io/tutorials/
+My Youtube Channel: https://www.youtube.com/user/MorvanZhou
+Dependencies:
+tensorflow: 1.1.0
+matplotlib
+"""
+import tensorflow as tf
+import numpy as np
+import matplotlib.pyplot as plt
+
+# fake data
+x = np.linspace(-5, 5, 200)     # x data, shape=(100, 1)
+
+# following are popular activation functions
+y_relu = tf.nn.relu(x)
+y_sigmoid = tf.nn.sigmoid(x)
+y_tanh = tf.nn.tanh(x)
+y_softplus = tf.nn.softplus(x)
+# y_softmax = tf.nn.softmax(x)  softmax is a special kind of activation function, it is about probability
+
+sess = tf.Session()
+y_relu, y_sigmoid, y_tanh, y_softplus = sess.run([y_relu, y_sigmoid, y_tanh, y_softplus])
+
+# plt to visualize these activation function
+plt.figure(1, figsize=(8, 6))
+plt.subplot(221)
+plt.plot(x, y_relu, c='red', label='relu')
+plt.ylim((-1, 5))
+plt.legend(loc='best')
+
+plt.subplot(222)
+plt.plot(x, y_sigmoid, c='red', label='sigmoid')
+plt.ylim((-0.2, 1.2))
+plt.legend(loc='best')
+
+plt.subplot(223)
+plt.plot(x, y_tanh, c='red', label='tanh')
+plt.ylim((-1.2, 1.2))
+plt.legend(loc='best')
+
+plt.subplot(224)
+plt.plot(x, y_softplus, c='red', label='softplus')
+plt.ylim((-0.2, 6))
+plt.legend(loc='best')
+
+plt.show()
